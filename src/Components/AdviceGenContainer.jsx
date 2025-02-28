1250492f7fd8cb3da351088b4bacb4c88a12c75b
xser56 <gettosheep56@gmail.com>
2025-02-28 14:14:42 -0800

    add alt tag

--- src/Components/AdviceGenContainer.jsx
+++ src/Components/AdviceGenContainer.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import "../App.css"
 
-
 const AdviceContainer = () => 
 {
     const [adviceID, setAdviceID] = useState('');
@@ -12,18 +11,14 @@ const AdviceContainer = () =>
     {
         const response = await fetch("https://api.adviceslip.com/advice");
         const data = await response.json();
-        console.log(data)
+        // console.log(data)
 
         setAdviceID(data.slip.id);
         setAdvice(`"${data.slip.advice}"`);
     }
 
+    //Resize function
     useEffect(() => 
-    {
-        grabAdviceData();
-    }, []);
-
-    useEffect(() => //Resize function
     {
         const handleResize = () => 
             setDividePart(window.innerWidth < 768 
@@ -35,17 +30,22 @@ const AdviceContainer = () =>
     
         return () => window.removeEventListener('resize', handleResize);
     }, []);
-    
+
+    useEffect(() => 
+    {
+        grabAdviceData();
+    }, []);
+
     return (
         <div className="w-[350px] md:w-[550px] h-auto max-h-[900px] bg-[#313A49] rounded-lg flex flex-col items-center relative">
 
-            <p className="text-[#52ffa8] font-medium text-[16px] my-10">A D V I C E  # {adviceID}</p>
+            <p className="text-[#52ffa8] font-medium text-[16px] my-10"> A D V I C E   # {adviceID}</p>
             <p className='text-[#cee3e9] font-semibold text-[28px] text-center w-[90%]'>{advice}</p>
 
             <img src={dividePart} alt="divider" className='mt-10 mb-20' />
 
             <div className="bg-[#52ffa8] rounded-[50%] h-[60px] w-[60px] flex items-center justify-center absolute bottom-[-30px] cursor-pointer hover:shadow-[0_0_40px] hover:shadow-[#52ffa8]" onClick={grabAdviceData}  >
-            <img src="../images/icon-dice.svg" alt="" />
+            <img src="../images/icon-dice.svg" alt="dice" />
             </div>
 
         </div>
